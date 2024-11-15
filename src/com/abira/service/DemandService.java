3c59e1c933b339c37c0fa1b5ec37290d2eeae924
Abira-Mandal26 <abiramandalofficial@gmail.com>
2024-11-15 22:41:36 +0530

    Update DemandService.java

--- src/com/abira/service/DemandService.java
+++ src/com/abira/service/DemandService.java
@@ -1,8 +1,8 @@
-package com.shashi.service;
+package com.abira.service;
 
 import java.util.List;
 
-import com.shashi.beans.DemandBean;
+import com.abira.beans.DemandBean;
 
 public interface DemandService {
 
